c385440716ed0d00b0af0e2b0a7587c1599852cb
loqmanali <grahyy1@gmail.com>
2024-10-13 04:24:43 +0700

    jsut update

--- example/flutter_package_renamer_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_package_renamer/flutter_package_renamer.dart';
-
-void main() async {
-  final updateConfig = UpdateConfig('example/config.json');
-  await updateConfig.run();
-}
